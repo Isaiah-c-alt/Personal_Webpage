0e60ee901aa7141b419267c400cc4951430c6ff0
Isaiah Cortez <isaiahcortez@Isaiahs-MBP.hitronhub.home>
2020-12-11 21:24:54 -0700

    Initial commit

---
+++ home-Script.js
@@ -0,0 +1,8 @@
+/* let name=prompt("Please enter your name: ");
+if (name="null")
+{
+  name="User";
+}
+alert("Welcome to my site, "+name);
+alert("The paragraphs are just filled with lorem ipsum text.");
+*/
